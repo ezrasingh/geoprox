0c83253f8528729612bfa0d6412de452e8b4201c
Ezra Singh <singhezra@gmail.com>
2024-07-15 20:01:13 -0400

    added some clap boilerplate

--- geoprox/src/main.rs
+++ geoprox/src/main.rs
@@ -1,11 +1,19 @@
 use clap::{Parser, Subcommand};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::{net::{IpAddr, Ipv4Addr, SocketAddr}, path::PathBuf};
 
 use geoprox_server;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+    /// specify a config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -13,7 +21,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// run web server
-    RunServer {
+    Run {
         /// <addr>:<port> (default 127.0.0.1:5000)
         #[arg(short, long)]
         bind: Option<SocketAddr>,
@@ -23,8 +31,19 @@ enum Commands {
 fn main() {
     let cli = Cli::parse();
 
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
     match &cli.command {
-        Some(Commands::RunServer { bind }) => {
+        Some(Commands::Run { bind }) => {
             geoprox_server::run(bind.unwrap_or(SocketAddr::new(
                 IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
                 5000,
