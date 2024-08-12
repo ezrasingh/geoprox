d47e4d5edf7b829da61b47df9ee2aaa92b75b6fe
Ezra Singh <singhezra@gmail.com>
2024-08-12 13:49:55 -0400

    Added tower-http layers to improve client-side experience (#13)

--- geoprox-server/src/config.rs
+++ geoprox-server/src/config.rs
@@ -3,13 +3,16 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 pub const DEFAULT_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
 pub const DEFAULT_PORT: u16 = 5000;
+pub const DEFAULT_TIMEOUT: u64 = 10;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct ServerConfig {
-    /// The address the server will bind to
+    /// The address the server will bind to (default 0.0.0.0)
     pub http_addr: Option<std::net::IpAddr>,
-    /// The port the server will listen on
+    /// The port the server will listen on (default 5000)
     pub http_port: Option<u16>,
+    /// Timeout duration in seconds (default 10)
+    pub timout: Option<u64>,
 }
 
 impl Default for ServerConfig {
@@ -17,6 +20,7 @@ impl Default for ServerConfig {
         Self {
             http_addr: Some(DEFAULT_ADDR),
             http_port: Some(DEFAULT_PORT),
+            timout: Some(DEFAULT_TIMEOUT),
         }
     }
 }
