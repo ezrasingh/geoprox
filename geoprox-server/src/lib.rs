548d91b299c2afdd4cbec01d0a288d2becc6fc2c
Ezra Singh <singhezra@gmail.com>
2024-08-23 01:29:59 -0400

    Added support for snapshots and persistence

--- geoprox-server/src/lib.rs
+++ geoprox-server/src/lib.rs
@@ -2,35 +2,30 @@ pub mod api;
 pub mod app;
 pub mod config;
 pub mod dto;
-pub mod handlers;
-pub mod middleware;
+
+mod handlers;
+mod middleware;
 
 use app::AppState;
 use config::ServerConfig;
 use geoprox_core::models::GeoShardConfig;
 
-use axum::routing::Router;
+use axum::routing::{IntoMakeService, Router};
 use axum::{body::Bytes, extract::Request};
 use std::time::Duration;
 use tower::Layer;
 use tower_http::compression::CompressionLayer;
 use tower_http::decompression::DecompressionLayer;
+use tower_http::normalize_path::NormalizePath;
 use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse};
 use tower_http::{normalize_path::NormalizePathLayer, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-/// Start http server
-#[tokio::main]
-pub async fn run(server_config: ServerConfig, shard_config: GeoShardConfig) {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "geoprox=debug".into()),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
-
+fn api_service(
+    server_config: ServerConfig,
+    shard_config: GeoShardConfig,
+) -> IntoMakeService<NormalizePath<Router>> {
     let state = AppState::new(server_config.clone(), shard_config);
     let router = Router::new()
         .nest("/api/v1/", api::routes(state))
@@ -51,25 +46,35 @@ pub async fn run(server_config: ServerConfig, shard_config: GeoShardConfig) {
                 )
                 // Set a timeout
                 .layer(TimeoutLayer::new(
-                    Duration::from_secs(server_config.timout.unwrap_or(config::DEFAULT_TIMEOUT)))
+                    server_config.timeout.unwrap().into())
                 )
-                // Compress responses
+                // Support response compression
                 .layer(CompressionLayer::new())
-                // Decompress requests
+                // Support Decompress requests
                 .layer(DecompressionLayer::new())
         );
 
     // normalize paths on all routes
     let router = NormalizePathLayer::trim_trailing_slash().layer(router);
+    axum::ServiceExt::<Request>::into_make_service(router)
+}
 
-    let socket: std::net::SocketAddr = server_config.into();
+/// Start http server
+#[tokio::main]
+pub async fn run(server_config: ServerConfig, shard_config: GeoShardConfig) {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "geoprox=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let socket: std::net::SocketAddr = server_config.clone().into();
     let listener = tokio::net::TcpListener::bind(socket).await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(
-        listener,
-        axum::ServiceExt::<Request>::into_make_service(router),
-    )
-    .await
-    .unwrap();
+    axum::serve(listener, api_service(server_config, shard_config))
+        .await
+        .unwrap();
 }
